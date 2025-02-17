e6f4b00399c982e064b217989b8d8b693cadac42
Xmchx <1394466835@qq.com>
2025-02-17 17:50:53 +0800

    update

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,15 @@
-use crate::Token;
+use crate::{Expr, Token};
 
 pub struct Parser<'a> {
     tokens: Vec<Token<'a>>,
     current: i32,
+}
+
+impl<'a> Parser<'a> {
+    fn expression(&mut self) -> Expr {
+        self.equality()
+    }
+
+    // equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+    fn equality(&mut self) -> Expr {}
 }
